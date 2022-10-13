dac78e26ee42149b9091fd3e5cbed92188560a11
Comparo <mati.agobian@gmail.com>
2022-10-13 12:11:31 -0300

    se agrega arrays

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,11 @@
 // debugger
 
 // Se Realiza una variable, donde se calcula la mayoría de edad.
-function entrada(){
-
+function entrada() {
   let nombre = prompt("ingrese su nombre, por favor: ");
   let edadUsr = parseInt(prompt("Ingrese su edad, por favor: "));
   const edad = 21;
-  
+
   if (edad <= edadUsr) {
     console.log("Bienvenido " + nombre + "!");
   } else {
@@ -16,8 +15,10 @@ function entrada(){
 entrada();
 
 //Se realiza un ciclo para elegir que tomar.
-function Drink(){
-  let bebida = prompt("Ingrese su bebida, puede ser por ejemplo FERNET o RON, para terminar ingrese: ESC");
+function Drink() {
+  let bebida = prompt(
+    "Ingrese su bebida, puede ser por ejemplo FERNET o RON, para terminar ingrese: ESC"
+  );
   while (bebida !== "ESC") {
     switch (bebida) {
       case "FERNET":
@@ -44,31 +45,63 @@ function Drink(){
         alert("No nos quedó 🤷‍♂️");
         break;
     }
-    bebida = prompt("Ingrese su bebida, para terminar ingrese: ESC")
+    bebida = prompt("Ingrese su bebida, para terminar ingrese: ESC");
   }
 }
 Drink();
 
+//SE AGREGA UN OBJETO
+class Trago {
+  constructor(nombre, compuesto, acompaniado, garnish, precio) {
+    this.nombre = nombre.toUpperCase();
+    this.compuesto = compuesto;
+    this.acompañado = acompaniado;
+    this.garnish = garnish;
+    this.precio = parseFloat(precio);
+  }
+  precioHappyHour() {
+    this.precio = this.precio / 1.5;
+  }
+}
 
-//SE AGREGA UN OBJETO 
-class Trago{
-  constructor(nombre, compuesto, acompaniado, garnish, precio ){
-   this.nombre = nombre.toUpperCase();
-   this.compuesto = compuesto;
-   this.acompañado = acompaniado;
-   this.garnish = garnish;
-   this.precio = parseFloat(precio);
- }
- precioHappyHour(){
-  this.precio = this.precio / 1.5;
- }
-} 
+const trago1 = new Trago(
+  "Old fashioned",
+  "Bourbon",
+  "Angostura",
+  "Rodaja de naranja",
+  1100
+);
+const trago2 = new Trago(
+  "Fernet con Coca",
+  "Fernet Branca",
+  "Coca-Cola",
+  "Espuma y Hielo",
+  800
+);
 
-const trago1 = new Trago("Old fashioned", "Bourbon", "Angostura", "Rodaja de naranja", 1100);
-const trago2 = new Trago("Fernet con Coca", "Fernet Branca", "Coca-Cola", "Espuma y Hielo", 800);
-
-//SE AGREGA UN ARRAY
+// SE AGREGA UN ARRAY
 const carta = [trago1, trago2];
-carta.push(new Trago("Garibaldi", "Campari", "Jugo de Naranja", "Rodaja de Naranja", 850));
+carta.push(
+  new Trago("Garibaldi", "Campari", "Jugo de Naranja", "Rodaja de Naranja", 850)
+);
 
 for (const producto of carta) producto.precioHappyHour();
+
+const drinks = [
+  { id: 1, tragoNew: "Negroni Sbagliato", price: 400 },
+  { id: 2, tragoNew: "Old Fashioned", price: 900 },
+  { id: 3, tragoNew: "Sol Y Sombra", price: 1000 },
+  { id: 4, tragoNew: "Sex On The Beach", price: 350 },
+];
+
+function buscarProducto() {
+  let buscar = prompt("Ingresa el producto a buscar. \nLas opciones son: Negroni Sbagliato, Old Fashioned, Sol Y Sombra, Sex On The Beach");
+
+  let resultadoTrago = drinks.find(drinks => drinks.tragoNew === (buscar));
+      if (resultadoTrago === undefined) {
+          console.warn("No se encontró el producto")
+      } else {
+          console.log(resultadoTrago)
+      }
+}
+buscarProducto();
